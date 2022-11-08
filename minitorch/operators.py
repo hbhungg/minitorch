6d310c87438c20758da2503cdbd6824ad317dacb
hung <bahung1tt@gmail.com>
2022-11-08 13:17:22 +1100

    task 0.1 all basic maths ops
    Dependencies now just grab the latest.

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -10,127 +10,120 @@ import math
 # Implementation of a prelude of elementary functions.
 
 
-def mul(x, y):
-    ":math:`f(x, y) = x * y`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def mul(x: float, y: float) -> float:
+  ":math:`f(x, y) = x * y`"
+  return x*y
 
 
-def id(x):
-    ":math:`f(x) = x`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def id(x: float) -> float:
+  ":math:`f(x) = x`"
+  return x
 
 
-def add(x, y):
-    ":math:`f(x, y) = x + y`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def add(x: float, y: float) -> float:
+  ":math:`f(x, y) = x + y`"
+  return x + y
 
 
-def neg(x):
-    ":math:`f(x) = -x`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def neg(x: float) -> float:
+  ":math:`f(x) = -x`"
+  return -x
 
 
-def lt(x, y):
-    ":math:`f(x) =` 1.0 if x is less than y else 0.0"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def lt(x: float, y: float) -> float:
+  ":math:`f(x) =` 1.0 if x is less than y else 0.0"
+  return x < y
 
 
-def eq(x, y):
-    ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def eq(x: float, y: float) -> float:
+  ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
+  return x == y
 
 
-def max(x, y):
-    ":math:`f(x) =` x if x is greater than y else y"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def max(x: float, y: float) -> float:
+  ":math:`f(x) =` x if x is greater than y else y"
+  if x > y: return x
+  return y
 
 
 def is_close(x, y):
-    ":math:`f(x) = |x - y| < 1e-2` "
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+  ":math:`f(x) = |x - y| < 1e-2` "
+  return math.isclose(x, y, rel_tol=1e-2)
 
 
-def sigmoid(x):
-    r"""
-    :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}`
+def sigmoid(x: float) -> float:
+  r"""
+  :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}`
 
-    (See `<https://en.wikipedia.org/wiki/Sigmoid_function>`_ .)
+  (See `<https://en.wikipedia.org/wiki/Sigmoid_function>`_ .)
 
-    Calculate as
+  Calculate as
 
-    :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}` if x >=0 else :math:`\frac{e^x}{(1.0 + e^{x})}`
+  :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}` if x >=0 else :math:`\frac{e^x}{(1.0 + e^{x})}`
 
-    for stability.
+  for stability.
 
-    Args:
-        x (float): input
+  Args:
+    x (float): input
 
-    Returns:
-        float : sigmoid value
-    """
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+  Returns:
+    float : sigmoid value
+  """
+  if x >= 0:
+    return 1.0/(math.exp(-x) + 1.0)
+  return math.exp(x)/(1.0+math.exp(x))
 
 
-def relu(x):
-    """
-    :math:`f(x) =` x if x is greater than 0, else 0
+def relu(x: float) -> float:
+  """
+  :math:`f(x) =` x if x is greater than 0, else 0
 
-    (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
+  (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
 
-    Args:
-        x (float): input
+  Args:
+    x (float): input
 
-    Returns:
-        float : relu value
-    """
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+  Returns:
+    float : relu value
+  """
+  if x > 0:
+    return x
+  return 0
 
 
 EPS = 1e-6
 
 
-def log(x):
-    ":math:`f(x) = log(x)`"
-    return math.log(x + EPS)
+def log(x: float) -> float:
+  ":math:`f(x) = log(x)`"
+  return math.log(x + EPS)
 
 
-def exp(x):
-    ":math:`f(x) = e^{x}`"
-    return math.exp(x)
-
-
-def log_back(x, d):
-    r"If :math:`f = log` as above, compute :math:`d \times f'(x)`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+def exp(x: float) -> float:
+  ":math:`f(x) = e^{x}`"
+  return math.exp(x)
 
 
 def inv(x):
-    ":math:`f(x) = 1/x`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+  ":math:`f(x) = 1/x`"
+  return 1/x
+
+
+def log_back(x, d):
+  r"If :math:`f = log` as above, compute :math:`d \times f'(x)`"
+  return d * (1/x+EPS)
 
 
 def inv_back(x, d):
-    r"If :math:`f(x) = 1/x` compute :math:`d \times f'(x)`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+  r"If :math:`f(x) = 1/x` compute :math:`d \times f'(x)`"
+  return d * (-1/math.pow(x, 2))
 
 
 def relu_back(x, d):
-    r"If :math:`f = relu` compute :math:`d \times f'(x)`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+  r"If :math:`f = relu` compute :math:`d \times f'(x)`"
+  if x > 0:
+    return d
+  return 0
 
 
 # ## Task 0.3
@@ -139,84 +132,84 @@ def relu_back(x, d):
 
 
 def map(fn):
-    """
-    Higher-order map.
+  """
+  Higher-order map.
 
-    .. image:: figs/Ops/maplist.png
+  .. image:: figs/Ops/maplist.png
 
 
-    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
+  See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
 
-    Args:
-        fn (one-arg function): Function from one value to one value.
+  Args:
+    fn (one-arg function): Function from one value to one value.
 
-    Returns:
-        function : A function that takes a list, applies `fn` to each element, and returns a
-        new list
-    """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  Returns:
+    function : A function that takes a list, applies `fn` to each element, and returns a
+    new list
+  """
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
 
 
 def negList(ls):
-    "Use :func:`map` and :func:`neg` to negate each element in `ls`"
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  "Use :func:`map` and :func:`neg` to negate each element in `ls`"
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
 
 
 def zipWith(fn):
-    """
-    Higher-order zipwith (or map2).
+  """
+  Higher-order zipwith (or map2).
 
-    .. image:: figs/Ops/ziplist.png
+  .. image:: figs/Ops/ziplist.png
 
-    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
+  See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
 
-    Args:
-        fn (two-arg function): combine two values
+  Args:
+    fn (two-arg function): combine two values
 
-    Returns:
-        function : takes two equally sized lists `ls1` and `ls2`, produce a new list by
-        applying fn(x, y) on each pair of elements.
+  Returns:
+    function : takes two equally sized lists `ls1` and `ls2`, produce a new list by
+    applying fn(x, y) on each pair of elements.
 
-    """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  """
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
 
 
 def addLists(ls1, ls2):
-    "Add the elements of `ls1` and `ls2` using :func:`zipWith` and :func:`add`"
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  "Add the elements of `ls1` and `ls2` using :func:`zipWith` and :func:`add`"
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
 
 
 def reduce(fn, start):
-    r"""
-    Higher-order reduce.
+  r"""
+  Higher-order reduce.
 
-    .. image:: figs/Ops/reducelist.png
+  .. image:: figs/Ops/reducelist.png
 
 
-    Args:
-        fn (two-arg function): combine two values
-        start (float): start value :math:`x_0`
+  Args:
+    fn (two-arg function): combine two values
+    start (float): start value :math:`x_0`
 
-    Returns:
-        function : function that takes a list `ls` of elements
-        :math:`x_1 \ldots x_n` and computes the reduction :math:`fn(x_3, fn(x_2,
-        fn(x_1, x_0)))`
-    """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  Returns:
+    function : function that takes a list `ls` of elements
+    :math:`x_1 \ldots x_n` and computes the reduction :math:`fn(x_3, fn(x_2,
+    fn(x_1, x_0)))`
+  """
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
 
 
 def sum(ls):
-    "Sum up a list using :func:`reduce` and :func:`add`."
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  "Sum up a list using :func:`reduce` and :func:`add`."
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
 
 
 def prod(ls):
-    "Product of a list using :func:`reduce` and :func:`mul`."
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+  "Product of a list using :func:`reduce` and :func:`mul`."
+  # TODO: Implement for Task 0.3.
+  raise NotImplementedError('Need to implement for Task 0.3')
