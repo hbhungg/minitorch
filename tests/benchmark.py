05ee282b780e65e6f4225bbe77a0cddd019cd9c0
hung <bahung1tt@gmail.com>
2022-11-09 22:08:54 +1100

    reindent all files to 2 spaces

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -6,10 +6,10 @@ backend = minitorch.make_tensor_backend(minitorch.CudaOps, is_cuda=True)
 
 total = 0
 for j in range(50):
-    a = minitorch.rand((1000, 1000), backend=backend)
-    start = time.time()
-    a.sum(1)
-    end = time.time()
-    if j != 0:
-        total += end - start
+  a = minitorch.rand((1000, 1000), backend=backend)
+  start = time.time()
+  a.sum(1)
+  end = time.time()
+  if j != 0:
+    total += end - start
 print(total / 50)
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -6,52 +6,52 @@ import minitorch
 
 
 class Network(minitorch.Module):
-    def __init__(self):
-        super().__init__()
-        self.layer = ScalarLinear(2, 1)
+  def __init__(self):
+    super().__init__()
+    self.layer = ScalarLinear(2, 1)
 
 
 class Network2(minitorch.Module):
-    def __init__(self):
-        super().__init__()
-        self.layer1 = ScalarLinear(2, 2)
-        self.layer2 = ScalarLinear(2, 1)
+  def __init__(self):
+    super().__init__()
+    self.layer1 = ScalarLinear(2, 2)
+    self.layer2 = ScalarLinear(2, 1)
 
 
 class ScalarLinear(minitorch.Module):
-    def __init__(self, in_size, out_size):
-        super().__init__()
-        self.weights = []
-        self.bias = []
-        for i in range(in_size):
-            self.weights.append([])
-            for j in range(out_size):
-                self.weights[i].append(
-                    self.add_parameter(
-                        f"weight_{i}_{j}", minitorch.Scalar(2 * (random.random() - 0.5))
-                    )
-                )
-        for j in range(out_size):
-            self.bias.append(
-                self.add_parameter(
-                    f"bias_{j}", minitorch.Scalar(2 * (random.random() - 0.5))
-                )
-            )
+  def __init__(self, in_size, out_size):
+    super().__init__()
+    self.weights = []
+    self.bias = []
+    for i in range(in_size):
+      self.weights.append([])
+      for j in range(out_size):
+        self.weights[i].append(
+          self.add_parameter(
+            f"weight_{i}_{j}", minitorch.Scalar(2 * (random.random() - 0.5))
+          )
+        )
+    for j in range(out_size):
+      self.bias.append(
+        self.add_parameter(
+          f"bias_{j}", minitorch.Scalar(2 * (random.random() - 0.5))
+        )
+      )
 
-    def forward(self, inputs):
-        y = [b.value for b in self.bias]
-        for i, x in enumerate(inputs):
-            for j in range(len(y)):
-                y[j] = y[j] + x * self.weights[i][j].value
-        return y
+  def forward(self, inputs):
+    y = [b.value for b in self.bias]
+    for i, x in enumerate(inputs):
+      for j in range(len(y)):
+        y[j] = y[j] + x * self.weights[i][j].value
+    return y
 
 
 @given(lists(scalars(), max_size=10), integers(min_value=5, max_value=20))
 def test_linear(inputs, out_size):
-    lin = ScalarLinear(len(inputs), out_size)
-    mid = lin.forward(inputs)
-    lin2 = ScalarLinear(out_size, 1)
-    lin2.forward(mid)
+  lin = ScalarLinear(len(inputs), out_size)
+  mid = lin.forward(inputs)
+  lin2 = ScalarLinear(out_size, 1)
+  lin2.forward(mid)
 
 
 # @given(
@@ -70,18 +70,18 @@ def test_linear(inputs, out_size):
 
 
 def test_nn_size():
-    model = Network2()
-    assert len(model.parameters()) == (
-        len(model.layer1.parameters()) + len(model.layer2.parameters())
-    )
+  model = Network2()
+  assert len(model.parameters()) == (
+    len(model.layer1.parameters()) + len(model.layer2.parameters())
+  )
 
-    assert model.layer2.bias[0].value.data != 0
-    assert model.layer1.bias[0].value.data != 0
-    assert model.layer1.weights[0][0].value.data != 0
+  assert model.layer2.bias[0].value.data != 0
+  assert model.layer1.bias[0].value.data != 0
+  assert model.layer1.weights[0][0].value.data != 0
 
-    for p in model.parameters():
-        p.update(minitorch.Scalar(0))
+  for p in model.parameters():
+    p.update(minitorch.Scalar(0))
 
-    assert model.layer2.bias[0].value.data == 0
-    assert model.layer1.bias[0].value.data == 0
-    assert model.layer1.weights[0][0].value.data == 0
+  assert model.layer2.bias[0].value.data == 0
+  assert model.layer1.bias[0].value.data == 0
+  assert model.layer1.weights[0][0].value.data == 0
